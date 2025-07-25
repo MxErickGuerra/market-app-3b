488b7bcc44f135d5d393ba1af19cfd8f99bc1d40
MxErickGuerra <mxguerre01@gmail.com>
2025-07-25 08:53:07 -0600

    Docs: document Product endpoint with Swagger annotations

--- src/main/java/com/tecdesoftware/market/web/controller/ProductController.java
+++ src/main/java/com/tecdesoftware/market/web/controller/ProductController.java
@@ -2,6 +2,13 @@ package com.tecdesoftware.market.web.controller;
 
 import com.tecdesoftware.market.domain.Product;
 import com.tecdesoftware.market.domain.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jdk.jfr.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +18,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
+@Tag(name = "Product controller", description = "Manage products in the store")
 public class ProductController {
 
     @Autowired
@@ -18,13 +26,29 @@ public class ProductController {
 
     // GET /products
     @GetMapping
+    @Operation(
+            summary = "Get all products",
+            description = "Return a list of all available products"
+    )
+    @ApiResponse(responseCode = "200", description = "Successful retrieval of products")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<List<Product>> getAll() {
         return ResponseEntity.ok(productService.getAll());
     }
 
     // GET /products/{productId}
     @GetMapping("/{productId}")
-    public ResponseEntity<Product> getProduct(@PathVariable("productId") int productId) {
+    @Operation(
+            summary = "Get product by Id",
+            description = "Return a product by its ID if exists"
+    )
+    @ApiResponse(responseCode = "200", description = "Product found")
+    @ApiResponse(responseCode = "404", description = "Product not found")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
+    public ResponseEntity<Product> getProduct(
+            @Parameter(description="Id of the product to be retrieved",
+                    example = "7",required = true)
+            @PathVariable("productId") int productId) {
         return productService.getProduct(productId)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
@@ -32,7 +56,18 @@ public class ProductController {
 
     // GET /products/category/{categoryId}
     @GetMapping("/category/{categoryId}")
-    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId) {
+    @Operation(
+            summary = "Get product by Category",
+            description = "Return a products in a specific category"
+    )
+    @ApiResponse(responseCode = "200", description = "Products found in the category")
+    @ApiResponse(responseCode = "204", description = "No products found in the category")
+    @ApiResponse(responseCode = "404", description = "Category not found")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
+    public ResponseEntity<List<Product>> getByCategory(
+            @Parameter(description="Category ID",
+                    example = "7",required = true)
+            @PathVariable("categoryId") int categoryId) {
         return productService.getByCategory(categoryId)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
@@ -40,6 +75,35 @@ public class ProductController {
 
     // POST /products
     @PostMapping
+    @Operation(
+            summary = "Save a new product",
+            description = "Registsters a new product and returns the created product",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    required = true,
+                    content=@Content(
+                            examples =@ExampleObject(
+                                    name= "Example Product",
+                                    value= """
+                                            {
+                                              "name": "cafe",
+                                              "categoryId": 0,
+                                              "price": 123,
+                                              "stock": 1,
+                                              "active": true,
+                                              "category": {
+                                                "categoryId": 8,
+                                                "category": "despensa",
+                                                "active": true
+                                              }
+                                            }
+                                            """
+                            )
+                    )
+            )
+
+    )
+    @ApiResponse(responseCode = "200", description = "Product post")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<Product> save(@RequestBody Product product) {
         return ResponseEntity.ok(productService.save(product));
     }
