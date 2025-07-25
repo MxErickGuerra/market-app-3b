fdab4a8c9357a0ee397ee91389d66f3e10816e2e
MxErickGuerra <mxguerre01@gmail.com>
2025-07-25 11:00:57 -0600

    Feat: allow CORS from all origins final

---
+++ src/test/java/com/tecdesoftware/market/MarketApp3bApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tecdesoftware.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketApp3bApplicationTests {
+
+	//version final?
+	public static void main(String[] args) {
+		SpringApplication.run(MarketApp3bApplicationTests.class,args);
+	}
+
+}
